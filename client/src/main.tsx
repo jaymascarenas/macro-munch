58be6154148753f8197d85ef9d6b0cff0faf7262
Andrew Birmingham <andrew_birmingham@msn.com>
2025-02-19 15:06:48 -0700

    added the sign up code and blended with what we already had

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,18 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-import App from './App.tsx';
+import App from "./App.tsx";
 
-import ErrorPage from './pages/ErrorPage.tsx';
-import Home from './pages/Home.tsx';
-import Login from './pages/Login.tsx';
-import Signup from './pages/Signup.tsx';
-import RecipeFinder from './pages/recipeFinder.tsx';
+import ErrorPage from "./pages/ErrorPage.tsx";
+import Home from "./pages/Home.tsx";
+import Login from "./pages/Login.tsx";
+import Signup from "./pages/Signup.tsx";
+import RecipeFinder from "./pages/recipeFinder.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
@@ -21,22 +21,22 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: <Login />,
       },
       {
-        path: '/signup',
+        path: "/signup",
         element: <Signup />,
       },
       {
-        path: 'recipeFinder',
+        path: "recipeFinder",
         element: <RecipeFinder />,
       },
     ],
   },
 ]);
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
